417bb971568f4a3b338268a799738618b0b36dfe
Kimhj0360 <hyunjong0360@gmail.com>
2022-02-11 14:42:55 +0900

    callback test

--- callbackTest.js
+++ callbackTest.js
@@ -1,4 +1,4 @@
- function run() {
+ /*function run() {
     const start = Date.now();
     for (let i = 0; i < 1000000000; i++) {}
     const end = Date.now();
@@ -7,4 +7,71 @@
 
   run();
   console.log('Done !!!');
+*/
 
+/*
+function run() {
+    setTimeout(() => {
+      const start = Date.now();
+      for (let i = 0; i < 1000000000; i++) {}
+      const end = Date.now();
+      console.log(end - start + 'ms');
+    }, 1000);
+  }
+ 
+  run();
+  console.log('Done !!!');
+*/
+
+/*
+  function run(callback) {
+   setTimeout(() => {
+     const start = Date.now();
+     for (let i = 0; i < 1000000000; i++) {}
+     const end = Date.now();
+     console.log(end - start + 'ms');
+     callback();
+   }, 1000);
+ }
+
+ run(() => {
+   console.log('Done !!!');
+ });
+*/
+
+/*
+function run() {
+   return new Promise((resolve, reject) => {
+       setTimeout(() => {
+           const start = Date.now();
+           for (let i = 0; i < 1000000000; i++) {}
+           const end = Date.now();
+           console.log(end - start + 'ms');
+           resolve();
+       }, 1000);
+   });
+}
+
+run().then(() => {console.log('Done !!!');});
+run().then(() => {return run()}).then(() => {console.log('Done !!!');});
+*/
+
+function run() {
+   return new Promise((resolve, reject) => {
+       setTimeout(() => {
+           const start = Date.now();
+           for (let i = 0; i < 1000000000; i++) {}
+           const end = Date.now();
+           console.log(end - start + 'ms');
+           resolve();
+       }, 1000);
+   });
+}
+
+async function process() {
+   await run();
+   await run();
+   console.log('Done !!!');
+}
+
+process();
